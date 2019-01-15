e31f205d8e4dbcee469b0b6983a17dd8bee6ffe1
RaenonX JELLYCAT <maplestoryxenon20130710@gmail.com>
2019-01-14 21:49:10 -0800

    Complete insert slot function

--- src/Board.java
+++ src/Board.java
@@ -1,13 +1,16 @@
-import sun.reflect.generics.reflectiveObjects.NotImplementedException;
-
 import java.util.ArrayList;
+import java.util.function.IntFunction;
+import java.util.function.Predicate;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Board {
     private ArrayList<ArrayList<Byte>> sudoku;
+    private Predicate<Byte> filterEmptySlots;
 
     Board(ArrayList<ArrayList<Byte>> sudoku) {
         this.sudoku = sudoku;
+        this.filterEmptySlots = x -> x != -1;
     }
 
     InsertResult insertSlot(int row, int column, byte number) {
@@ -23,25 +26,55 @@ public class Board {
     }
 
     boolean validInSquare(int row, int column, byte number) {
-        throw new NotImplementedException();
+        IntFunction<Integer> blockMin = x -> x * 2;
+        IntFunction<Integer> blockMax = x -> blockMin.apply(x) + 2;
+
+        int rowBlock = Math.floorDiv(row, 3);
+        int colBlock = Math.floorDiv(column, 3);
+
+        ArrayList<Byte> al = new ArrayList<Byte>() {{
+            add(number);
+        }};
+
+        for (int r = blockMin.apply(rowBlock); r < blockMax.apply(rowBlock); r++) {
+            for (int c = blockMin.apply(colBlock); c < blockMax.apply(colBlock); c++) {
+                al.add(sudoku.get(r).get(c));
+            }
+        }
+
+        return validArrayList(al);
     }
 
     boolean validInRow(int row, byte number) {
-        throw new NotImplementedException();
+        return validArrayList(new ArrayList<Byte>(sudoku.get(row)) {{
+            add(number);
+        }});
     }
 
     boolean validInColumn(int column, byte number) {
-        throw new NotImplementedException();
+        ArrayList<Byte> al = new ArrayList<Byte>() {{
+            add(number);
+        }};
+        for (int row = 0; row < 9; row++) al.add(sudoku.get(row).get(column));
+
+        return validArrayList(al);
+    }
+
+    boolean validArrayList(ArrayList<Byte> al) {
+        Stream<Byte> s = al.stream().filter(this.filterEmptySlots);
+
+        return s.distinct().count() == s.count();
     }
 
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-
-        for (ArrayList<Byte> row : sudoku) {
-            sb.append(row.stream().map(x -> x == -1 ? BoardReader.EMPTY_SYMBOL : x.toString()).map(Object::toString).collect(Collectors.joining()));
-        }
-
-        return sb.toString();
+        return sudoku
+                .stream()
+                .map(row -> row
+                        .stream()
+                        .map(x -> x == -1 ? BoardReader.EMPTY_SYMBOL : x.toString())
+                        .map(Object::toString)
+                        .collect(Collectors.joining()))
+                .collect(Collectors.joining("\n"));
     }
 }
 
