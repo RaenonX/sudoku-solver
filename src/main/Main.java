d3af2b4296ea98d7c7a6d870b68035397567d48e
RaenonX JELLYCAT <raenonx0710@gmail.com>
2019-01-26 03:57:45 -0800

    Create test for solve - check valid

--- src/main/Main.java
+++ src/main/Main.java
@@ -2,7 +2,7 @@ package main;
 
 public class Main {
     public static void main(String[] args) {
-        Board b = BoardReader.readSafe("puzzle-normal.sdk");
+        Board b = BoardReader.readSafe("puzzle-board.ss");
 
         System.out.println(b);
         System.out.println(b.autoSolve());
